f66144f58fad23cb4145b751db987a1d1edf94b8
ClementinePringarbe <clementine.pringarbe@laposte.net>
2022-10-25 21:07:09 +0200

    Affichage de l'accueil et des produits

--- src/Controller/Front/MainController.php
+++ src/Controller/Front/MainController.php
@@ -6,15 +6,65 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Repository\ProductRepository;
+
 class MainController extends AbstractController
 {
     /**
-     * @Route("/front/main", name="app_front_main")
+     * @Route("/", name="home")
      */
-    public function index(): Response
+    public function home(): Response
     {
-        return $this->render('front/main/index.html.twig', [
+        return $this->render('front/main/home.html.twig', [
             'controller_name' => 'MainController',
         ]);
+    }
+
+    /**
+     * @Route("/list", name="list", methods={"GET"})
+     * 
+     * @return Response
+     */
+    public function list(ProductRepository $productRepository): Response
+    {
+        $listProducts = $productRepository->findAllProductsByNameAscQb();
+
+        // On transmet à notre vue la liste des produits
+        return $this->render(
+            'front/main/list.html.twig',
+            ["listProducts" => $listProducts]
+        );
+    }
+
+    /**
+     * @Route(
+     *      "/product/{id}", 
+     *      name="productShow", 
+     *      methods={"GET"}, 
+     *      requirements={"id"="\d+"})
+     * 
+     * @param int $id 
+     * @return Response
+     */
+    public function productShow(int $id, ProductRepository $productRepository): Response
+    {
+        // On utilise le ProductRepository
+        $dataProduct = $productRepository->find($id);
+
+        // Si l'id contient un index qui n'existe pas
+        if (is_null($dataProduct)) {
+
+            // on lance une exception qui est particulière
+            // puisqu'elle renvoie aussi au navigateur un status HTTP 404
+            throw $this->createNotFoundException('Le produit n\'existe pas.');
+        }
+
+        // on renvoie le template twig dans lequel on transmet les données du produit demandé en paramètre
+        return $this->render(
+            'main/product-show.html.twig',
+            [
+                'product' => $dataProduct
+            ]
+        );
     }
 }
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -39,6 +39,44 @@ class ProductRepository extends ServiceEntityRepository
         }
     }
 
+    //* DQL : Doctrine Query Language
+    // On veut créer une méthode qui nous renvoie tous les produits triés par leurs noms par ordre alphabétique
+    public function findAllProductsByNameAscDQL()
+    {
+        $entityManager = $this->getEntityManager();
+
+        // En DQL, on ne fait pas de requête sur les tables directement, mais sur les entités Doctrine
+        //?https://www.doctrine-project.org/projects/doctrine-orm/en/2.11/reference/dql-doctrine-query-language.html#dql-select-examples
+        // On va donc utiliser le FQCN des entités à l'intérieur de la requête DQL
+        // En SQL :  SELECT * FROM product ORDER BY name ASC
+        $query = $entityManager->createQuery('
+            SELECT p FROM App\Entity\Product p ORDER BY p.name ASC
+        ');
+
+        return $query->getResult();
+    }
+
+    //* Query Builder
+    
+    /**
+     * On veut créer une méthode qui nous renvoie tous les produits triés par leurs noms par ordre alphabétique
+     *
+     * @return mixed
+     */
+    public function findAllProductsByNameAscQb()
+    {
+        // Le querybuilder sait déjà qu'on va requêter sur l'entité Product
+        // car nous sommes dans ProductRepository
+        // Donc pas besoin de préciser le FQCN de l'entité à requêter
+        // 'p' est juste l'alias de App\Entity\Product
+        $results = $this->createQueryBuilder('p') 
+            ->orderBy('p.name', 'ASC') // On trie sur la propriété creditOrder
+            ->getQuery()
+            ->getResult();
+
+        return $results;
+    }
+
 //    /**
 //     * @return Product[] Returns an array of Product objects
 //     */
